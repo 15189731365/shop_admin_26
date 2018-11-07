8dba54cd79d4f30d1feba949d7a70343389446fa
15189731365 <1418595096@qq.com>
2018-11-07 22:03:19 +0800

    初步完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 导入ElementUI的组件库
+import ElementUI from 'element-ui'
+// 导入其样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
